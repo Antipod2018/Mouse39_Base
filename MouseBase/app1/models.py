b7c9ad080634143c993ec4771f393fe09a228893
Valentin_Mouse39 <47462272+Antipod2018@users.noreply.github.com>
2022-05-25 16:31:58 +0200

    создал базу данных для типов товаров, привязал к общей базе

--- MouseBase/app1/models.py
+++ MouseBase/app1/models.py
@@ -11,6 +11,7 @@ class notebook(models.Model):
     time_update = models.DateTimeField(auto_now=True)
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
     status = models.BooleanField(default=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.title
@@ -18,8 +19,12 @@ class notebook(models.Model):
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
 
+
 class Category(models.Model):
-    name=models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True)
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'cat_id': self.pk})
--- MouseBase/app1/views.py
+++ MouseBase/app1/views.py
@@ -10,12 +10,29 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
 ]
 def index(request):
     posts = notebook.objects.all()
+    cats = Category.objects.all()
     context = {
         'posts': posts,
+        'cats': cats,
         'menu': menu,
-        'title': 'Mouse39'
+        'title': 'Mouse39',
+        'cat_selected': 0,
     }
     return render(request, 'notebooks/index.html', context=context)
+
+def show_category(request, cat_id):
+    posts = notebook.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Отображение по товарам',
+        'cat_selected': cat_id,
+    }
+    return render(request, 'notebooks/index.html', context=context)
+
 
 def about(request):
     return render(request, 'notebooks/about.html', {'menu': menu , 'title': 'о приложении'})
@@ -34,3 +51,4 @@ def show_post(request, post_id):
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Not found</h1>')
+
